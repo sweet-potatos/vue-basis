d7c6df881aa6da7809b7a12c62f65dc3b0d965b7
xuzhuquan <xuzhuquan@dfl.com.cn>
2022-05-22 16:47:00 +0800

    feat: 添加vue-basis项目

---
+++ 17.单文件组件/School.vue
@@ -0,0 +1,27 @@
+<template>
+	<!-- 组件结构 html -->
+	<div>
+		<h2>{{name}}</h2>
+		<h2>{{address}}</h2>
+	</div>
+</template>
+
+<script>
+	// 组件交互相关的代码(数据、方法等)
+	const School = {
+		//组件定义时，一定不要写el配置项，因为最终所有的组件都要被一个vm接管，由vm决定服务于哪个容器
+		name: 'School',
+		data(){
+			return {
+				name: '中大',
+				address: '广州',
+			}
+		},
+	}
+	
+	export default School;
+</script>
+
+<style>
+	/* 组件的样式 */
+</style>
---
+++ 17.单文件组件/Student.vue
@@ -0,0 +1,26 @@
+<template>
+	<!-- 组件结构 html -->
+	<div>
+		<h2>{{name}}</h2>
+		<h2>{{age}}</h2>
+	</div>
+</template>
+
+<script>
+	// 组件交互相关的代码(数据、方法等)
+	const Student = {
+		name: 'Student',
+		data(){
+			return {
+				name: '张三',
+				age: 18,
+			}
+		},
+	}
+	
+	export default Student;
+</script>
+
+<style>
+	/* 组件的样式 */
+</style>
